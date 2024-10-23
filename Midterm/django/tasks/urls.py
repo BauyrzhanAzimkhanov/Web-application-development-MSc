a30b947f347a745e177149c718a8c814a54b05b1
BauyrzhanAzimkhanov <azimkhanov4@gmail.com>
2024-10-24 04:50:35 +0500

    feat: added details, index views

--- Midterm/django/tasks/urls.py
+++ Midterm/django/tasks/urls.py
@@ -7,8 +7,16 @@ urlpatterns = [
     path("", views.index, name="index"),
     # ex: /tasks/5/
     path("<int:task_id>/", views.detail, name="detail"),
-    # ex: /tasks/5/results/
-    path("<int:task_id>/results/", views.results, name="results"),
+    # ex: /tasks/5/edit/
+    path("<int:task_id>/edit/", views.edit, name="edit"),
     # ex: /tasks/5/finish/
-    path("<int:task_id>/finish/", views.finish, name="vote"),
+    path("<int:task_id>/finish/", views.finish, name="finish"),
+    # ex: /tasks/5/incomplete/
+    path("<int:task_id>/incomplete/", views.incomplete, name="incomplete"),
+    # ex: /tasks/create/
+    path("create/", views.create, name="create"),
+    # ex: /tasks/created/
+    path("created/", views.created, name="created"),
+    # ex: /tasks/5/delete/
+    path("<int:task_id>/delete/", views.delete, name="delete"),
 ]
--- Midterm/django/tasks/views.py
+++ Midterm/django/tasks/views.py
@@ -2,34 +2,54 @@ from django.shortcuts import render, get_object_or_404
 from django.http import Http404
 from django.http import HttpResponse
 from django.template import loader
+from django.utils import timezone
 
 from .models import Task
 
 def index(request):
-    latest_tasks_list = Task.objects.order_by("-created_at")[:5]
+    latest_tasks_list = Task.objects.order_by("-created_at")
     template = loader.get_template("tasks/index.html")
     context = {
         "latest_tasks_list": latest_tasks_list,
     }
-    # output = ", ".join([q.title for q in latest_tasks_list])
-    # return HttpResponse(output)
-    # return HttpResponse(template.render(context, request))
     return render(request, "tasks/index.html", context)
 
 def detail(request, task_id):
-    # try:
-    #     task = Task.objects.get(pk=task_id)
-    # except Task.DoesNotExist:
-    #     raise Http404("task does not exist")
-    # return HttpResponse("You're looking at task %s." % task_id)
     task = get_object_or_404(Task, pk=task_id)
     return render(request, "tasks/detail.html", {"task": task})
 
-
-def results(request, task_id):
-    response = "You're looking at the results of task %s."
-    return HttpResponse(response % task_id)
-
-
 def finish(request, task_id):
+    task = get_object_or_404(Task, pk=task_id)
+    task.completed = True
+    task.save()
+    return render(request, "tasks/finished.html", {"task": task})
+
+def incomplete(request, task_id):
+    task = get_object_or_404(Task, pk=task_id)
+    task.completed = False
+    task.save()
+    return render(request, "tasks/incompleted.html", {"task": task})
+
+def create(request):
+    return render(request, "tasks/task_form.html")
+
+def created(request):
+    if request.method == "GET":
+        title = request.GET.get("title")
+        description = request.GET.get("description")
+        completed = request.GET.get("completed")
+        if completed == None:
+            completed = False
+        else:
+            completed = True
+        task = Task(title = title, description = description, created_at = timezone.now(), completed = completed)
+        task.save()
+        return render(request, "tasks/detail.html", {"task": task})
+
+def edit(request, task_id):
     return HttpResponse("You're finished task %s." % task_id)
+
+
+def delete(request, task_id):
+    return HttpResponse("You're finished task %s." % task_id)
+    
