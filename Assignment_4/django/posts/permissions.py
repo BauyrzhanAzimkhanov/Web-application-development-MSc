1f78b0e2da5d1725d6cde77d83867ec4462b0b1f
BauyrzhanAzimkhanov <azimkhanov4@gmail.com>
2024-11-28 00:42:14 +0500

    fix: assignment 4

--- Assignment_4/django/posts/permissions.py
+++ Assignment_4/django/posts/permissions.py
@@ -1,8 +1,12 @@
 from rest_framework import permissions
+from .models import Post
 
 class IsAuthorOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
-        else:
-            return obj.author == request.user
+        
+        if isinstance(obj, Post): 
+            return obj.author == request.user.username 
+        
+        return False
---
+++ Assignment_4/django/references.md
@@ -0,0 +1,5 @@
+https://www.geeksforgeeks.org/swagger-integration-with-python-django/
+https://stackoverflow.com/questions/51182823/django-rest-framework-nested-serializers
+https://www.django-rest-framework.org/api-guide/permissions/
+https://www.django-rest-framework.org/api-guide/versioning/
+https://www.django-rest-framework.org/api-guide/viewsets/
